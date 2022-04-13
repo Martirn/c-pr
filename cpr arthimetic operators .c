46e2dcf38b037d22866eedfca10fac40d682b349
Martirn <102363832+Martirn@users.noreply.github.com>
2022-04-13 13:24:09 +0300

    Add files via upload
    Assignment

---
+++ cpr arthimetic operators .c
@@ -0,0 +1,15 @@
+// program output for arthimetic operators
+#include<stdio.h>
+
+int main()
+{
+ int u=2,w=3,v,y,x,t,j,k;
+v=u+w;
+y=v*u;
+x=y-v;
+t=w/u; 
+j=y%w; 
+k=v+x+y+t+j ;  
+    printf("sum of 5 numbers is %d\n",k);
+    return 0;
+}
