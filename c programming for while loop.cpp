36e415518c8bc338462b8eaa388cecbce361a6d6
Martirn <102363832+Martirn@users.noreply.github.com>
2022-04-13 11:17:08 +0300

    Add files via upload
    C programming assignment

---
+++ c programming for while loop.cpp
@@ -0,0 +1,49 @@
+// a program to output first 50 numbers
+
+# include <stdio.h>
+int main()
+{
+int  n= 50;
+while(n> 0)
+{
+printf("%d \n",n);
+n-= 1;
+}
+printf("end of loop");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
