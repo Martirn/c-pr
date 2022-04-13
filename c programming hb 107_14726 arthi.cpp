a0867a07d941bbcb833060f46a23e3f127505694
Martirn <102363832+Martirn@users.noreply.github.com>
2022-04-13 11:21:42 +0300

    Add files via upload
    Assignment

---
+++ c programming hb 107_14726 arthi.cpp
@@ -0,0 +1,54 @@
+//arthematic operator
+# include <stdio.h>
+int main()
+{
+int  a=6, b=8,c;
+c= a+b;
+printf("sum=%d\n",c);
+c=a-b;
+printf("difference=%d\n",c);
+c= a*b;
+printf("product=%d\n",c);
+c=a%b;
+printf("remainder=%d\n",c);
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
---
+++ c.programming.hb107_g_14726_21.no3.cpp
@@ -0,0 +1,23 @@
+//rational operator
+# include <stdio.h>
+int main()
+{
+
+int quantity ;
+float price;
+float TotalExpenses;
+printf("Enter the quantity purchased\n");
+scanf("%f, &price");
+if(quantity>1000);
+{TotalExpenses= quantity*price;
+TotalExpenses=TotalExpenses-(TotalExpenses*0.1);
+}
+
+{
+TotalExpenses=quantity*price;
+}
+printf("TotalExpenses is: %f", TotalExpenses);
+
+
+return 0;
+}
