38b6994ad2c7132ac9cb3d7f98b6f835cb8c70de
Martirn <102363832+Martirn@users.noreply.github.com>
2022-04-14 08:15:25 +0300

    Add files via upload
    Assignment

---
+++ cpr rational operator to check even and odd.c
@@ -0,0 +1,28 @@
+//rational operator
+#include <stdio.h>
+int main()
+{
+ int number;
+ printf("Enter a Value: ");
+ scanf("%d", &number);
+ if(number % 2 == 0)
+  printf("\n%d is an even Number", number);
+    else
+  printf("\n%d is an odd Number", number);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
